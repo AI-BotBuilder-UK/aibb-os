6f173a014f250dc574f526cd4342ff7a0eb73db8
Phillip-Llewellyn <phil@aibotbuilders.co.uk>
2025-09-13 16:16:49 +0100

    Update main.jsx to render App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
-function App() {
-  return (
-    <h1>Hello, AI BotBuilders OS is live!</h1>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App />
+);
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
