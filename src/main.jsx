87b89adefac153355549b9a00003d1f43ebb6a95
Phillip-Llewellyn <phil@aibotbuilders.co.uk>
2025-09-13 18:31:14 +0100

    Update main.jsx to load App with variant

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
+
+// Read variant flag from the HTML page (set in dfy.html, dwy.html, diy.html, book.html)
+const variant = window.__VARIANT__ || "dfy";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+  <React.StrictMode>
+    <App variant={variant} />
+  </React.StrictMode>
 );
-
